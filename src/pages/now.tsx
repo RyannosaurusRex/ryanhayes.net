8a10c9ac0fb5f5a13fdcd9de54a6511ebf136ad0
Ryan Hayes <ryan@ryanhayes.net>
2019-04-19 18:50:10 -0400

    Added shell of a /now page.

---
+++ src/pages/now.tsx
@@ -0,0 +1,50 @@
+import IndexLayout from '../layouts';
+import Wrapper from '../components/Wrapper';
+import SiteNav from '../components/header/SiteNav';
+import { SiteHeader, outer, inner, SiteMain } from '../styles/shared';
+import * as React from 'react';
+import { css } from '@emotion/core';
+
+import { PostFullHeader, PostFullTitle, NoImage, PostFull } from '../templates/post';
+import { PostFullContent } from '../components/PostContent';
+import Footer from '../components/Footer';
+import Helmet from 'react-helmet';
+
+const PageTemplate = css`
+  .site-main {
+    background: #fff;
+    padding-bottom: 4vw;
+  }
+`;
+
+
+const About: React.FunctionComponent = () => (
+  <IndexLayout>
+    <Helmet>
+      <title>Now</title>
+    </Helmet>
+    <Wrapper css={PageTemplate}>
+      <header css={[outer, SiteHeader]}>
+        <div css={inner}>
+          <SiteNav />
+        </div>
+      </header>
+      <main id="site-main" className="site-main" css={[SiteMain, outer]}>
+        <article className="post page" css={[PostFull, NoImage]}>
+          <PostFullHeader>
+            <PostFullTitle>Now</PostFullTitle>
+          </PostFullHeader>
+
+          <PostFullContent className="post-full-content">
+          <div className="post-content">
+            //TODO
+		      </div>
+          </PostFullContent>
+        </article>
+      </main>
+      <Footer />
+    </Wrapper>
+  </IndexLayout>
+);
+
+export default About;
