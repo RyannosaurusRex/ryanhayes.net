c6229ace02d1169637aa0f26cd3652ccd96e6a96
Ryan Hayes <ryan@ryanhayes.net>
2019-04-18 23:29:49 -0400

    Added google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,13 +73,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-15608898-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
         anonymize: true,
         // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
+        respectDNT: false,
         // Avoids sending pageview hits from custom paths
         exclude: ['/preview/**'],
         // Specifies what percentage of users should be tracked
