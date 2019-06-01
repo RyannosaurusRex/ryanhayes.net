df4e0df6f47ee1421e3c56f71c4be6b3bd6f1399
Ryan Hayes <Jason.Hayes@e-hps.com>
2019-05-31 20:35:29 -0400

    Added contact form.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -15,6 +15,28 @@ const PageTemplate = css`
     background: #fff;
     padding-bottom: 4vw;
   }
+
+  .contact-form {
+    font-family: inherit;
+  }
+
+  .button {
+    border-radius: 3px;
+  }
+  
+  .form-wrapper {
+    max-width: 600px;
+    margin-top: 15px;
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
+  
+  textarea {
+    width:100%;
+    height:150px;
+    font-size: 80%;
+  }
 `;
 
 
@@ -36,17 +58,26 @@ const Contact: React.FunctionComponent = () => (
           </PostFullHeader>
 
           <PostFullContent className="post-full-content">
-            <form name="contact" netlify>
-              <p>
-                <label>Name <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Email <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
+            Need to get in touch? I make it a point to respond to every comment
+            on this blog and social media. If email is more appropriate, just fill
+            out this form and I'll get back to you.
+            <div className="form-wrapper">
+              <form className="contact-form" name="contact" data-netlify-recaptcha="true" netlify>
+                <p>
+                  <label>Name <input type="text" name="name" /></label>
+                </p>
+                <p>
+                  <label>Email <input type="email" name="email" /></label>
+                </p>
+                <p>
+                  <label for="message">Message</label><br/><textarea id="message" name="message" />
+                </p>
+                <div data-netlify-recaptcha="true"></div>
+                <p>
+                  <button className="button" type="submit">Send</button>
+                </p>
+              </form>
+            </div>
           </PostFullContent>
         </article>
       </main>
