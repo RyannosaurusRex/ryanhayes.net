9f2b6362362390a4ce97cf37e3aa584d5dec1210
Ryan Hayes <ryanhayes84@gmail.com>
2019-10-29 21:32:06 -0400

    Small now page update.

--- src/website-config.ts
+++ src/website-config.ts
@@ -62,7 +62,7 @@ const config: WebsiteConfig = {
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  footer: '',
 };
 
 export default config;
